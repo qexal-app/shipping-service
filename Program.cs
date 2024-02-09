49f5e4bfa6c0f980838d0d22061652ad496061b2
vmural <vitalii.mural@gmail.com>
2024-02-09 20:52:43 +0200

    Create shipping-provider.proto

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
+using System.Net;
 using ManualShippingProvider.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddGrpc();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.Listen(IPAddress.Loopback, 7070, listenOptions =>
+        {
+            listenOptions.Protocols = HttpProtocols.Http2;
+            listenOptions.UseHttps();
+        });
+    });
+}
 
 var app = builder.Build();
 
--- Services/ShippingProviderService.cs
+++ Services/ShippingProviderService.cs
@@ -1,13 +1,28 @@
+using Google.Protobuf;
 using Grpc.Core;
 using Qexal;
 
 namespace ManualShippingProvider.Services;
 
-public class ShippingProviderService(ILogger<ShippingProviderService> logger) : ShippingProvider.ShippingProviderBase
+public class ShippingProviderService(IWebHostEnvironment environment) : ShippingProvider.ShippingProviderBase
 {
-    public override Task<ShippingLabelResponse> Label(ShippingLabelRequest request, ServerCallContext context)
+    public override Task<ValidateAddressResponse> ValidateAddress(ValidateAddressRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new ShippingLabelResponse { DocumentUrl = "https://google.com" });
+        var result = new ValidateAddressResponse
+        {
+            Status = AddressValidationStatus.Verified,
+            OriginalAddress = request.Address,
+            MatchedAddress = request.Address,
+            ServiceName = request.ServiceName
+        };
+
+        return Task.FromResult(result);
+    }
+
+    public override async Task<ShippingLabelResponse> Label(ShippingLabelRequest request, ServerCallContext context)
+    {
+        var bytes = await File.ReadAllBytesAsync(Path.Combine(environment.WebRootPath, "label.pdf"));
+        return new ShippingLabelResponse { Document = ByteString.CopyFrom(bytes) };
     }
 
     public override Task<ShippingRegisterResponse> Save(ShippingRegisterRequest request, ServerCallContext context)
@@ -16,7 +31,7 @@ public class ShippingProviderService(ILogger<ShippingProviderService> logger) : 
         {
             Shipment = new ShipmentResponse
             {
-                TrackingNumber = request.OrderNumber
+                TrackingNumber = "1234 5678 9012 3456 7890 12"
             }
         };
 
